60212cfcd0c515cd29a21b3581f2c985761a074b
Julien Hwang <jhwang@lcs.on.ca>
2025-03-05 08:30:38 -0500

    Made the interface so it shows the quotes

--- QuoteFinder/View/qouteView.swift
+++ QuoteFinder/View/qouteView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct qouteView: View {
+    
+    //MARK: Stored properties
+    
+    //Create the View model
+    @State var viewModel = QuoteViewModel()
+    
+    @State var buttonOpacity = 0.0
+    // Starts a timer to wait on revealing button to get new joke
+    @State var buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //Show quotes
+            if let currentQuote = viewModel.currentQuote {
+                Group {
+                    Text(currentQuote.quoteText)
+                        .padding()
+                    
+                }
+            }
         }
         .padding()
     }
