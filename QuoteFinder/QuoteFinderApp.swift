de993c7bdf254acdcc5241714f64320d58b4ceb7
Julien Hwang <jhwang@lcs.on.ca>
2025-03-04 19:51:01 -0500

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by Julien Hwang on 4/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
